629106fd1c1d051645bfef5c70b7256aeb15e8b4
Peter Caisse <pcaisse@gmail.com>
2023-02-19 18:36:47 -0500

    Add global styles
    See: https://github.com/vercel/next-learn/blob/master/basics/assets-metadata-css-starter/styles/globals.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 
+import "../styles/global.css";
+
 const CanvassingApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
