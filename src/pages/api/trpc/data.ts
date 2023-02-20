83148caf6889a97476b0e1df1b85a6de75ccccea
Peter Caisse <pcaisse@gmail.com>
2023-02-20 10:09:45 -0500

    Require name and note to save

--- src/pages/api/trpc/data.ts
+++ src/pages/api/trpc/data.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
+export const hasId = z.object({ id: z.number() });
+
 export const saveNoteData = z.object({
   id: z.number().optional(),
-  name: z.string(),
-  note: z.string(),
+  name: z.string().min(1, { message: "Name is required" }),
+  note: z.string().min(1, { message: "Note is required" }),
   createdAt: z.date().optional(),
   updatedAt: z.date().nullish(),
 });
